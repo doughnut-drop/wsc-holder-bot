143468f6b0536c5067a10caac7da9aa87a71e8eb
doughnut-drop <doughnut6716@protonmail.com>
2022-04-03 02:33:28 +0800

    add pseudo code for non holders

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 import ethscan_api
 import web3_functions
@@ -12,6 +13,8 @@ class DiscordClient(discord.Client):
     async def on_ready(self):
         self.eth_client = web3_functions.EthClient()
         self.ethscan = ethscan_api.EthScan()
+        self.date_format = "%Y-%m-%d"
+        self.current_date = datetime.strptime("2022-03-31", self.date_format)
 
         # get sabi (staking) contract object
         self.staking_contract_address = settings.STAKING_CONTRACT_ADDRESS
@@ -30,16 +33,16 @@ class DiscordClient(discord.Client):
     async def eth_connection(self):
         await self.message.channel.send(self.eth_client.is_connected())
 
-    def get_wsc_token_count(self):
-        wallet_address = self.msg.split(" ")[-1]
+    def get_wsc_token_count(self, wallet_address):
         staked_list = self.eth_client.get_wsc_token_count(self.staking_contract_obj, self.count_staking_function, wallet_address)  # get staked count from address; returns list(int)
         unstaked_count = self.eth_client.get_wsc_token_count(self.unstaked_contract_obj, self.count_unstaked_function, wallet_address)  # get staked count from address; returns int
         total_wsc_token_owned = len(staked_list) + unstaked_count
         return total_wsc_token_owned
 
     async def grant_role(self):
+        wallet_address = self.msg.split(" ")[-1]
         member = self.message.author
-        token_count = self.get_wsc_token_count()
+        token_count = self.get_wsc_token_count(wallet_address)
 
         role_id = None
         for holder in settings.HOLDER_ROLES:
@@ -65,7 +68,27 @@ class DiscordClient(discord.Client):
 
         embedMsg.add_field(name="total WSC tokens found", value=token_count, inline=False)
         await self.message.channel.send(embed=embedMsg)
-        await self.message.channel.send("Holder role has been granted.")
+        await self.message.channel.send(f"<@{member.id}>: `{role.name}` & `{default_role.name}` roles have been granted.")
+
+    async def remove_non_holders(self):
+        await self.message.channel.send("Starting the check for non holders...")
+        owners_data = [
+            {"discord_id": 882534422065319977, "wallet_address": "0x310421C955b9a714Ad7a86C1c57c9698FD962318"},
+        ]
+        # check total token staked per address
+        holder_roles_to_remove = [discord.utils.get(bot.get_all_members(), id=owner_data["discord_id"]) for owner_data in owners_data if self.get_wsc_token_count(owner_data["wallet_address"]) == 0]
+        holder_role_ids = [x["role_id"] for x in settings.HOLDER_ROLES]
+
+        for member in holder_roles_to_remove:
+            for member_role in member.roles:
+                if member_role.id in holder_role_ids:
+                    role = discord.utils.get(member.guild.roles, id=member_role.id)
+                    await member.remove_roles(role, reason="user is no longer a holder.")
+
+            # remove default holder role
+            default_role = discord.utils.get(member.guild.roles, id=settings.DEFAULT_HOLDER_ROLE)
+            await member.remove_roles(default_role, reason="user is no longer a holder.")
+        await self.message.channel.send("Holder roles have been removed from non holders.")
 
     async def on_message(self, message):
 
@@ -74,6 +97,14 @@ class DiscordClient(discord.Client):
 
         self.message = message
         self.msg = message.content
+
+        # # check to remove non-holders
+        # message_date = datetime.strptime(message.created_at.strftime(self.date_format), self.date_format)
+        # if message_date > self.current_date:
+        #     self.current_date = date_today
+        #     await self.update_holders()
+        #     await self.remove_non_holders()
+
         if self.msg.startswith("!verify"):
             await self.grant_role()
 
